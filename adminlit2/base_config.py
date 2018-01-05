c74903a3ecc19f83b107be4a8e746d56b82614de
rainard <30309517+rainard@users.noreply.github.com>
2018-01-05 15:33:23 +0800

    Update base_config.py

--- adminlit2/base_config.py
+++ adminlit2/base_config.py
@@ -17,13 +17,13 @@ dev_config={
 }
 
 product_config={
-    'debug' : False ,
-    'db_host' : '10.21.248.248',
-    'db_name'  : 'go2',
+    'debug' : True,
+    'db_host' : '127.0.0.1',
+    'db_name'  : 'adminlit2',
     'db_user' : 'root',
     'db_password' : 'E9%bEb3bcbcF2E8D^D',
     'db_port' : 3306 ,
-    'redis_host' :  '10.21.248.248',
+    'redis_host' : '127.0.0.1',
     'redis_port' : 6379,
     'redis_passwd' : '64978f1dd5fb',
     'redis_db' : 1 ,
